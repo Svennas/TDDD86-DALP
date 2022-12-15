aea7599efa6032788c156facfcc48c5e8c0e3280
Gustav Svennas <gussv907@student.liu.se>
2022-12-15 21:46:56 +0100

    Started with lab 8.

---
+++ lab8/trailblazer/src/trailblazer.cpp
@@ -0,0 +1,45 @@
+// This is the CPP file you will edit and turn in.
+// Also remove these comments here and add your own, along with
+// comments on every function and on complex code sections.
+// TODO: write comment header for this file; remove this comment
+
+#include "costs.h"
+#include "trailblazer.h"
+// TODO: include any other headers you need; remove this comment
+using namespace std;
+
+vector<Node *> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
+    // TODO: implement this function; remove these comments
+    //       (The function body code provided below is just a stub that returns
+    //        an empty vector so that the overall project will compile.
+    //        You should remove that code and replace it with your implementation.)
+    vector<Vertex*> path;
+    return path;
+}
+
+vector<Node *> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
+    // TODO: implement this function; remove these comments
+    //       (The function body code provided below is just a stub that returns
+    //        an empty vector so that the overall project will compile.
+    //        You should remove that code and replace it with your implementation.)
+    vector<Vertex*> path;
+    return path;
+}
+
+vector<Node *> dijkstrasAlgorithm(BasicGraph& graph, Vertex* start, Vertex* end) {
+    // TODO: implement this function; remove these comments
+    //       (The function body code provided below is just a stub that returns
+    //        an empty vector so that the overall project will compile.
+    //        You should remove that code and replace it with your implementation.)
+    vector<Vertex*> path;
+    return path;
+}
+
+vector<Node *> aStar(BasicGraph& graph, Vertex* start, Vertex* end) {
+    // TODO: implement this function; remove these comments
+    //       (The function body code provided below is just a stub that returns
+    //        an empty vector so that the overall project will compile.
+    //        You should remove that code and replace it with your implementation.)
+    vector<Vertex*> path;
+    return path;
+}
