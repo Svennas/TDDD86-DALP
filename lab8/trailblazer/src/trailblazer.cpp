2f5f36eae576180c6f6463e9ba919c42dc431d50
Gustav Svennas <gussv907@student.liu.se>
2022-12-16 11:14:29 +0100

    Lab 8: Started on dfs. Trying to get a grasp of all the code and information related to the lab.

--- lab8/trailblazer/src/trailblazer.cpp
+++ lab8/trailblazer/src/trailblazer.cpp
@@ -8,11 +8,13 @@
 // TODO: include any other headers you need; remove this comment
 using namespace std;
 
-vector<Node *> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
+vector<Node *> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end)
+{
+    start->visited = true;
+    vector<Node> neighbours;
+
+
+
     vector<Vertex*> path;
     return path;
 }
