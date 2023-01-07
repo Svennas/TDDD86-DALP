6aef8678c696a69d5f6e4c87025cc8f62a694c64
Gustav Svennas <gussv907@student.liu.se>
2023-01-07 22:42:13 +0100

    Lab 8: Working on BFS.

--- lab8/trailblazer/src/trailblazer.cpp
+++ lab8/trailblazer/src/trailblazer.cpp
@@ -5,6 +5,7 @@
 
 #include "costs.h"
 #include "trailblazer.h"
+#include "queue.h"
 // TODO: include any other headers you need; remove this comment
 using namespace std;
 
@@ -42,12 +43,46 @@ void dFSRecursive(BasicGraph& graph, Vertex* curr, Vertex* end, vector<Vertex*>&
     curr->setColor(GRAY);
 }
 
-vector<Node *> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
+vector<Node *> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end)
+{
+    graph.resetData();
     vector<Vertex*> path;
+
+    if (start == nullptr) return path;
+
+    Queue<Vertex*> vertexQ;
+
+    start->visited = true;
+    path.push_back(start);
+    vertexQ.enqueue(start);
+
+    cout << "Start: " << start->name << endl;
+
+    while (path.back() != end)
+    {
+        Vertex* curr = vertexQ.dequeue();
+        curr->setColor(GREEN);
+        path.push_back(curr);
+
+        cout << "Current: " << curr->name << endl;
+
+        for (Vertex* near : graph.getNeighbors(curr))
+        {
+            if (!near->visited)
+            {   // Add all the neighbors not visited to the queue
+                cout << "Neighbors: " << near->name << endl;
+                near->visited = true;
+                near->setColor(YELLOW); // Node is in queue
+                vertexQ.enqueue(near);
+            }
+
+        }
+
+
+
+
+    }
+
     return path;
 }
 
